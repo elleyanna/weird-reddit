698cc8a751d4ae8cead2c91202a28efcf3a4c0bb
Elleanna Dwyer <elleanna.tech@gmail.com>
2020-07-01 06:09:38 -0400

    Card item cleanup

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -3,11 +3,9 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 
@@ -23,13 +21,7 @@ const CardItem = ({ file, icon }) => {
     <Card className={classes.root}>
       <CardActionArea>
         <CardHeader
-          avatar={
-            <Avatar
-              aria-label="recipe"
-              src={icon}
-              // className={classes.avatar}
-            />
-          }
+          avatar={<Avatar aria-label="subreddit avatar" src={icon} />}
           title={file.data.title}
           subheader={`r/${file.data.subreddit}`}
         />
