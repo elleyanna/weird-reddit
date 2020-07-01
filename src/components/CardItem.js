d2ba5721f5d03a2724d593a87bee068a536ad26b
“elleyanna” <elleanna.tech@gmail.com>
2020-07-01 08:47:37 -0400

    Hooked up card items to link out to artwork

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -4,6 +4,7 @@ import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import Avatar from "@material-ui/core/Avatar";
 import CardActionArea from "@material-ui/core/CardActionArea";
+import Link from "@material-ui/core/Link";
 import CardActions from "@material-ui/core/CardActions";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
@@ -13,32 +14,48 @@ const useStyles = makeStyles({
   root: {
     maxWidth: 345,
   },
+  buttonBase: {
+    display: "block",
+    textAlign: "initial",
+  },
 });
 
 const CardItem = ({ file, icon }) => {
   const classes = useStyles();
+
+  const preventDefault = (event) => event.preventDefault();
+
   return (
     <Card className={classes.root}>
       <CardActionArea>
-        <CardHeader
-          avatar={<Avatar aria-label="subreddit avatar" src={icon} />}
-          title={file.data.title}
-          subheader={`r/${file.data.subreddit}`}
-        />
-        <CardMedia
-          component={file.data.domain === "youtu.be" ? "iframe" : "img"}
-          src={
-            file.data.domain === "youtu.be"
-              ? `https://www.youtube.com/embed/${file.data.url
-                  .split("/")
-                  .pop()}`
-              : null
-          }
-          image={file.data.domain !== "youtu.be" ? file.data.url : null}
-          alt={file.data.url}
-          height="140"
-          title={file.data.title}
-        />
+        <Link
+          rel="noopener"
+          color="inherit"
+          target="_blank"
+          href={file.data.url}
+          underline="none"
+        >
+          <CardHeader
+            avatar={<Avatar aria-label="subreddit avatar" src={icon} />}
+            title={file.data.title}
+            subheader={`r/${file.data.subreddit}`}
+          />
+
+          <CardMedia
+            component={file.data.domain === "youtu.be" ? "iframe" : "img"}
+            src={
+              file.data.domain === "youtu.be"
+                ? `https://www.youtube.com/embed/${file.data.url
+                    .split("/")
+                    .pop()}`
+                : null
+            }
+            image={file.data.domain !== "youtu.be" ? file.data.url : null}
+            alt={file.data.url}
+            height="140"
+            title={file.data.title}
+          />
+        </Link>
       </CardActionArea>
       <CardActions>
         <ArrowUpwardIcon />
