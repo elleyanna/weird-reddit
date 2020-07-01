d1e0dab311fa3d95d9763cded1dcfadad05a2567
Elleanna Dwyer <elleanna.tech@gmail.com>
2020-06-30 20:23:16 -0400

    Introduced cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import AppBarTop from "./components/AppBarTop.js";
-import Card from "./components/Card.js";
+import CardCollection from "./components/CardCollection.js";
 import renAndStimpy from "./assets/renAndStimpy.png";
-
-import Grid from "@material-ui/core/Grid";
-import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    padding: "60px",
-    marginTop: "60px",
-    marginBottom: "60px",
-  },
   container: {
     backgroundImage: `url(${renAndStimpy})`,
     position: "absolute",
@@ -24,78 +15,49 @@ const useStyles = makeStyles((theme) => ({
     overflow: "auto",
     zIndex: "-1",
   },
-  cardContainer: {
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-  },
 }));
 
 function App() {
   const classes = useStyles();
+
+  const [items, setItems] = useState({ files: [], after: null, before: null });
+  const [subredditIcon, setSubredditIcon] = useState({
+    icon: null,
+  });
+
+  const redditCreepyArtAPI = useEffect(() => {
+    fetch(
+      "https://www.reddit.com/r/CreepyArt+UnusualArt+alternativeart+wtfart+cringe+diWHY/hot.json?count=25"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setItems({
+          files: data.data.children,
+          after: data.data.after,
+          before: data.data.before,
+        });
+        // window.scrollTo(0, 0);
+      });
+  }, []);
+
+  const redditSubredditArtAPI = useEffect(() => {
+    fetch("https://www.reddit.com/r/CreepyArt/about.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setSubredditIcon({
+          icon: data.data.icon_img,
+        });
+      });
+  }, []);
+
   return (
     <div>
       <div className={classes.container}>
         <AppBarTop />
-        <div className={classes.root}>
-          <Container>
-            <Grid
-              container
-              alignItems="flex-start"
-              justify="space-between"
-              spacing={3}
-            >
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-              <Grid item>
-                <Card />
-              </Grid>
-            </Grid>
-          </Container>
-        </div>
+        <CardCollection
+          files={items.files}
+          icon="https://www.reddit.com/r/CreepyArt/about.json"
+        />
       </div>
     </div>
   );
