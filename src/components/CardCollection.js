ae2aee7df0e555c0ed85a94c3d3493fe035b3079
jadejarosz0 <jjarosz@urbn.com>
2020-10-27 02:09:20 -0400

    style update

--- src/components/CardCollection.js
+++ src/components/CardCollection.js
@@ -8,7 +8,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    paddingTop: "120px",
+    paddingTop: "80px",
     paddingBottom: "60px",
   }
 }));
